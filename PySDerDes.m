0c56831d4c1be0390e1ea8b6adf5a76236a8b554
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-28 01:57:20 +0200

    comentada funcion

--- PySDerDes.m
+++ PySDerDes.m
@@ -7,8 +7,12 @@ function [yd, ydd] = PySDerDes(x, y)
 % Outputs:
 %   yd = vector fila con la primera derivada en los puntos
 %   ydd = vector fila con la segunda derivada en los puntos
-% Nota: Se deben introducir al menos 4 puntos.
-% Nota: x es la variable independiente respecto de la que se deriva.
+% Notas: 
+%   Por simplicidad se asume que el usuario no introduce x repetidas de 
+%   forma que a cada valor de x le corresponde un único valor de y.
+%   x es la variable independiente respecto de la que se deriva.
+
+    % Se comprueba que la nube de puntos introducida es válida
     n = numel(x);
     if n ~= numel(y)
         disp("Error: Debe introducir el mismo número de coords y que x")
@@ -16,25 +20,33 @@ function [yd, ydd] = PySDerDes(x, y)
     end
     if n < 4
         disp("Error: Debe introducir al menos 4 puntos")
+        return
     end
+    
+    % Expresión de la primera derivada para el polinomio de Lagrange de
+    % grado 2 para 3 puntos evaluada en xval
     d_lagrange = @(x, y, xval) ...
         2*xval-x(2)-x(3) / ((x(1)-x(2))*(x(1)-x(3))) * y(1) + ...
         2*xval-x(1)-x(3) / ((x(2)-x(1))*(x(2)-x(3))) * y(2) + ...
         2*xval-x(1)-x(2) / ((x(3)-x(1))*(x(3)-x(2))) * y(3);
 
+    % Se calcula la primera derivada en cada punto
     yd = zeros(1, n);
-
     yd(1) = d_lagrange(x(1:3), y(1:3), x(1));
     for i = 1: n-2
         yd(i+1) = d_lagrange(x(i:i+2), y(i:i+2), x(i+1));
     end
     yd(i+2) = d_lagrange(x(i:i+2), y(i:i+2), x(i+2));
 
+    % Expresión de la segunda derivada para el polinomio de Lagrange de
+    % grado 3 para 4 puntos evaluada en xval
     dd_lagrange = @(x, y, xval) ...
         6*xval-2*x(2)-2*x(3)-2*x(4) / ((x(1)-x(2))*(x(1)-x(3))*(x(1)-x(4))) * y(1) + ...
         6*xval-2*x(1)-2*x(3)-2*x(4) / ((x(2)-x(1))*(x(2)-x(3))*(x(2)-x(4))) * y(2) + ...
         6*xval-2*x(1)-2*x(2)-2*x(4) / ((x(3)-x(1))*(x(3)-x(2))*(x(3)-x(4))) * y(3) + ...
         6*xval-2*x(1)-2*x(2)-2*x(3) / ((x(4)-x(1))*(x(4)-x(2))*(x(4)-x(3))) * y(4);
+
+    % Se calcula la segunda derivada en cada punto
     ydd = zeros(1, n);
     ydd(1) = dd_lagrange(x(1:4), y(1:4), x(1));
     for i = 2: n-2
